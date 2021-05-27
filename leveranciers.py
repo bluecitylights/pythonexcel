effec7fb2cddaca6b900936b39b82e3f0beb67c9
bluecitylights <30653230+bluecitylights@users.noreply.github.com>
2021-05-27 20:52:11 +0200

    Update leveranciers.py

--- leveranciers.py
+++ leveranciers.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data_file = './data/leveranciers.xlsx'                                  # the input excel file
+data_file = '.\data\leveranciers.xlsx'                                 # the input excel file
 
 output_file_1 = './data/week1.csv'                                      # the name of first output file
 output_file_2 = './data/week2.csv'                                      # the name of second output file
@@ -19,9 +19,16 @@ def get_dax_for_week(data_file, output_file, columns, date):
         df3 = df2[['Magazijn', 'IGNR', 'Aantal', 'LEV NR', 'Haven']]    # select the correct columns for DAX
         df3.columns = ['Magazijn', 'Artikel', 'Aantal', 'leveranciersnummer', 'Haven']  # rename columns to DAX format
         datumcol = [date for x in range(len(df3.index))]
-        df3.insert(2, "Datum", datumcol)
-        df3.to_csv(output_file, sep='|', index=False)                           # write to csv file
+        df3.insert(3, "leverDatum", datumcol)
+        afsluitcol = ["" for x in range(len(df3.index))]
+        df3.insert(6, "", afsluitcol)
 
-get_dax_for_week(data_file, output_file_1, "A,D,F,G:I", "week33")               # create csv for first week (magazijn G:I)
-get_dax_for_week(data_file, output_file_2, "A,D,F,J:L", "week40")               # create csv for second week (magazijn J:L)
-get_dax_for_week(data_file, output_file_3, "A,D,F,M:O", "week45")               # create csv for third week (magazijn m:o)
+        niet_0 = df3["Aantal"]!=0
+        df4=df3[niet_0]
+        
+        df4["Aantal"] = df4["Aantal"].astype(int)
+        df4.to_csv(output_file, sep='|', index=False)                   # write to csv file
+
+get_dax_for_week(data_file, output_file_1, "A,D,F,G:I", "20211001")     # create csv for first week (magazijn G:I)
+get_dax_for_week(data_file, output_file_2, "A,D,F,J:L", "20211101")     # create csv for second week (magazijn J:L)
+get_dax_for_week(data_file, output_file_3, "A,D,F,M:O", "20211201")     # create csv for third week (magazijn m:o)
